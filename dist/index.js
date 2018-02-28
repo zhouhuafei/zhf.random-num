24a03676f9204fc8d8c68c803bb7a43c481aa155
zhouhuafei <zhouhuafei@shopex.cn>
2018-02-28 13:33:34 +0800

    object

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,29 @@
-"use strict";
+'use strict';
 
-function randomNum(min, max) {
-    if (!max) {
-        max = min;
-        min = 0;
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+(function (name, factory) {
+    if ((typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined') {
+        // nodejs - commonjs canon
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // requirejs - amd canon
+        define(factory);
+    } else {
+        // window - browser canon
+        if (Object.prototype.toString.call(window.zhf).slice(8, -1).toLowerCase() !== 'object') {
+            window.zhf = {};
+        }
+        window.zhf[name] = factory();
     }
-    return Math.round(Math.random() * (max - min) + min);
-}
+})('randomNum', function () {
+    function randomNum(min, max) {
+        if (!max) {
+            max = min;
+            min = 0;
+        }
+        return Math.round(Math.random() * (max - min) + min);
+    }
 
-module.exports = randomNum;
+    return randomNum;
+});
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-function randomNum(min, max) {
-    if (!max) {
-        max = min;
-        min = 0;
+(function (name, factory) {
+    if (typeof exports === 'object' && typeof module !== 'undefined') { // nodejs - commonjs canon
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) { // requirejs - amd canon
+        define(factory);
+    } else { // window - browser canon
+        if (Object.prototype.toString.call(window.zhf).slice(8, -1).toLowerCase() !== 'object') {
+            window.zhf = {};
+        }
+        window.zhf[name] = factory();
     }
-    return Math.round(Math.random() * (max - min) + min);
-}
+})('randomNum', function () {
+    function randomNum(min, max) {
+        if (!max) {
+            max = min;
+            min = 0;
+        }
+        return Math.round(Math.random() * (max - min) + min);
+    }
 
-module.exports = randomNum;
+    return randomNum;
+});
